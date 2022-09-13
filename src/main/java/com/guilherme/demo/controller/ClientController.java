74832fbce112ac63f7e54de9134adcc94ebb06ff
Roberto Alves <roberto@rhfactor.com.br>
2022-09-13 20:15:16 -0300

    Reforando os métodos para poder tirar repetição de codigo

--- src/main/java/com/guilherme/demo/controller/ClientController.java
+++ src/main/java/com/guilherme/demo/controller/ClientController.java
@@ -1,78 +1,51 @@
 package com.guilherme.demo.controller;
 
 import com.guilherme.demo.model.Client;
-import com.guilherme.demo.repository.ClientRepository;
+import com.guilherme.demo.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/clients/")
+@RequestMapping("clients")
 public class ClientController {
 
-    private final ClientRepository clientRepository;
+    @Autowired private ClientService clientService;
 
-    @Autowired
-    public ClientController(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
+    @GetMapping
+    public String clientForm(@ModelAttribute Client client) {
+        return "form-client";
     }
 
-    @GetMapping("signup")
-    public String showSignUpForm(Client client) {
-        return "add-client";
-    }
+    @GetMapping("{id}")
+    public String clientEditForm(@PathVariable("id") long id, Model model) {
+        Client client = clientService.getClient(id);
 
-    @GetMapping("list")
-    public String showUpdateForm(Model model) {
-        model.addAttribute("clients", clientRepository.findAll());
-        return "index";
-    }
-
-    @PostMapping("add")
-    public String addClient(@Valid Client client, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-client";
+        if( client == null ){
+            return "redirect:/";
         }
 
-        clientRepository.save(client);
-        return "redirect:list";
-    }
-
-    @GetMapping("edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        Client client = clientRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid client Id:" + id));
         model.addAttribute("client", client);
-        return "update-client";
+        return "form-client";
     }
 
-    @PostMapping("update/{id}")
-    public String updateClient(@PathVariable("id") long id, @Valid Client client, BindingResult result,
-                               Model model) {
+    @PostMapping
+    public String processForm(@Valid Client client, BindingResult result) {
         if (result.hasErrors()) {
-            client.setId(id);
-            return "update-client";
+            return "form-client";
         }
-
-        clientRepository.save(client);
-        model.addAttribute("students", clientRepository.findAll());
-        return "index";
+        clientService.save( client );
+        return "redirect:/";
     }
 
     @GetMapping("delete/{id}")
-    public String deleteClient(@PathVariable("id") long id, Model model) {
-        Client client = clientRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid client Id:" + id));
-        clientRepository.delete(client);
-        model.addAttribute("clients", clientRepository.findAll());
-        return "index";
+    public String processDelete(@PathVariable("id") Long id) {
+        clientService.delete(id);
+        return "redirect:/";
     }
 }
 
